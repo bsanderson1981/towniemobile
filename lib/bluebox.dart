d4dd324ca681f1e132edc70aafa10f2237461312
bsanderson1981 <bill@towniebagels.com>
2020-10-25 18:59:33 -0700

    blue dot

---
+++ lib/bluebox.dart
@@ -0,0 +1,81 @@
+
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyWidget(),
+    );
+  }
+}
+
+class MyWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        BlueBox(),
+        Flexible(
+          fit: FlexFit.loose,
+          flex: 1,
+          child: RedBox(),
+        ),
+        Fglexible(
+          fit: FlexFit.loose,
+          flex: 1,
+          child: GreenBox(),
+        ),
+      ],
+    );
+  }
+}
+
+class BlueBox extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 50,
+      height: 50,
+      decoration: BoxDecoration(
+        color: Colors.blue,
+        border: Border.all(),
+      ),
+    );
+  }
+}
+
+
+class GreenBox extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 50,
+      height: 50,
+      decoration: BoxDecoration(
+        color: Colors.green,
+        border: Border.all(),
+      ),
+    );
+  }
+}
+class RedBox extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 50,
+      height: 50,
+      decoration: BoxDecoration(
+        color: Colors.red,
+        border: Border.all(),
+      ),
+    );
+  }
+}
