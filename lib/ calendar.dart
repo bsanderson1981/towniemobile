33116a4878cd8a629a48dc3f22ff8410c0850640
bsanderson11 <bill@towniebagewls.com>
2020-11-05 10:08:04 -0800

    test new branch footer

--- lib/ calendar.dart
+++ lib/ calendar.dart
@@ -4,7 +4,7 @@ import 'package:flutter/material.dart';
 //TODO Block all tuesdays from pickup dates
 //TODO how to i track counts for 13 types of bagels and breads in window
 //TODO  in SnackBar menu: need bagel count to show bakers dozen   1 bdoz + 7 bagels total
-
+//test branch
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
